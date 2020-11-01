99f2b739db8bc9b22afbc747f8ebdebdd8fa46ac
sreeja99 <srijagodishala@gmail.com>
2020-11-01 16:03:24 +0530

    Added:Created A Method To Retrive All The Entries From Database

---
+++ firstmavenproject/src/test/java/com/capgemini/addressbook/AddressBookTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.addressbook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddressBookTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
